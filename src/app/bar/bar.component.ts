3dee867a8228c64dc67ba9270d6575f99e82e76a
romanutti <36601005+romanutti@users.noreply.github.com>
2022-11-24 20:44:40 +0100

    1 file modified, 3 added, 3 deleted

--- src/app/bar/bar.component.ts
+++ src/app/bar/bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { BazService } from 'src/app/baz.service';
+import { BazService } from '../baz.service';
 
 @Component({
   selector: 'app-bar',
